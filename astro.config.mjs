058065f959fddec4881a7df7246638e4bfce94ab
richiebotteri <richies-frontend-projects@outlook.com>
2025-09-16 12:09:20 +0200

    added Sanity + React integratino

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
+
 const LIVE_URL = 'https://richiebotteri.github.io'
 
 
 // https://astro.build/config
 export default defineConfig({
-    site: LIVE_URL
+  site: LIVE_URL,
+  integrations: [sanity({
+    projectId: "hauxx0m4",
+    dataset: "production",
+    useCdn: false, // for static builds
+  }), react()]
 });
